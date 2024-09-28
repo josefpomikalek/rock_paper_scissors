1d2b421aee86b7656dd8135f54e1ed4feaf130ff
josefpomikalek <josef_pomikalek@yahoo.com>
2024-09-28 18:45:05 +0200

    Add the condition IF solving "out of range" error.

--- main.py
+++ main.py
@@ -30,25 +30,29 @@ scissors = '''
 gestures = [rock, paper, scissors]
 
 user_choice = int(input("What do you chose? Type 0 for Rock, 1 for Paper and 2 for Scissors. "))
-print("You chose:")
-print(gestures[user_choice])
 
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(gestures[computer_choice])
+if user_choice > 2 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+else:
+    print("You chose:")
+    print(gestures[user_choice])
 
-if user_choice == computer_choice:
-    print("It's a draw!")
-elif user_choice == 0 and computer_choice == 1:
-    print("You lose!")
-elif user_choice == 0 and computer_choice == 2:       
-    print("You win!")
-elif user_choice == 1 and computer_choice == 0:
-    print("You win!")
-elif user_choice == 1 and computer_choice == 2:
-    print("You lose!")
-elif user_choice == 2 and computer_choice == 0:
-    print("You lose!")
-elif user_choice == 2 and computer_choice == 1:
-    print("You win!")
-    
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(gestures[computer_choice])
+
+    if user_choice == computer_choice:
+        print("It's a draw!")
+    elif user_choice == 0 and computer_choice == 1:
+        print("You lose!")
+    elif user_choice == 0 and computer_choice == 2:       
+        print("You win!")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You win!")
+    elif user_choice == 1 and computer_choice == 2:
+        print("You lose!")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You lose!")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You win!")
+        
